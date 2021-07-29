ebb2cb5b199f137e9f77400dac3ab59b09a1ff14
Seth <sethvance26@gmail.com>
2021-07-29 13:35:33 -0400

    displaying Payee names within App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header/Header';
-
-// const data = require ('./Data/sample.json')
+// import Header from './components/Header/Header';
+import Data from "./Data/sample.json";
 
 function App() {
   return (
-<div>
-  <Header/>
-
+   
+<div className="posts">
+  {Data.map(post => {
+    console.log(post)
+    
+    return(
+      <>
+      <p>{post.Payee.Name}</p>
+      
+      </>
+    )
+  })}
 </div>
+
   );
 }
+
 
 export default App;
